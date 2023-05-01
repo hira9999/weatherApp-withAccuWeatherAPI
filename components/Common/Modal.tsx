507a357cf364a2cc09f415455a1c897ca1ff99ee
hira9999 <iedddd@naver.com>
2023-05-01 22:41:55 +0900

    refactor: change name modalMessage => modalContent

--- components/Common/Modal.tsx
+++ components/Common/Modal.tsx
@@ -2,34 +2,34 @@ import React from 'react';
 
 interface ModalProps {
   isModalOpen: boolean;
-  modalMessage: {
+  modalContent: {
     [key: string]: string;
   };
   handleModal: () => void;
 }
 
-const Modal = ({ isModalOpen, modalMessage, handleModal }: ModalProps) => {
+const Modal = ({ isModalOpen, modalContent, handleModal }: ModalProps) => {
   return (
     <>
       <div
         className={`${
           isModalOpen ? 'scale-y-100 opacity-100' : 'scale-y-0 opacity-0'
-        } duration-150 rounded-lg absolute z-20 w-52 h-52 transition -right-[225px] -top-6 bg-[#262626] border-[#5D6C80] border-[1px] outline outline-[1px] outline-black shadow-md`}
+        } hidden sm:block duration-150 rounded-lg absolute z-20 w-52 h-52 transition -right-[225px] -top-6 bg-[#262626] border-[#5D6C80] border-[1px] outline outline-[1px] outline-black shadow-md`}
       >
         <div className="flex flex-col bg-[#262626] absolute z-20 w-full h-full rounded-lg scrollbar-hide overflow-y-scroll">
           {/* 툴팁 메시지가 표시되는곳 */}
           <div className="flex justify-center sticky top-0 bg-[#262626] text-white font-extrabold w-full p-1.5">
-            {modalMessage.title}
+            {modalContent.title}
           </div>
           <div className="space-y-1 p-3">
-            {Object.keys(modalMessage)
+            {Object.keys(modalContent)
               .slice(1)
               .map((key) => {
                 return (
                   <div key={key}>
                     <h3 className="font-bold mb-2">{key.replace(/_/g, ' ')}</h3>
                     <p className="text-xs text-[#D6DEE6]">
-                      {modalMessage[key]}
+                      {modalContent[key]}
                     </p>
                   </div>
                 );
