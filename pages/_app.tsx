243bfdb998d328feb665539be5db34f24797a07b
hira9999 <iedddd@naver.com>
2023-05-09 20:57:08 +0900

    feat: Add Head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,18 @@ import { client } from '../lib/apolloClient';
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 import APIErrorBoundary from '../components/Common/ApiErrorBoundary';
+import Head from '../components/Common/Head';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <APIErrorBoundary client={client}>
-      <ApolloProvider client={client}>
-        <Component {...pageProps} />
-      </ApolloProvider>
-    </APIErrorBoundary>
+    <>
+      <Head />
+      <APIErrorBoundary client={client}>
+        <ApolloProvider client={client}>
+          <Component {...pageProps} />
+        </ApolloProvider>
+      </APIErrorBoundary>
+    </>
   );
 }
 
