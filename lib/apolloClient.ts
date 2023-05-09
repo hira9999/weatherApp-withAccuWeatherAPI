fa807bfac0832557b0cb9ff6d83bb6139e6ce584
hira9999 <iedddd@naver.com>
2023-05-09 20:59:16 +0900

    fix: fix graphql uri

--- lib/apolloClient.ts
+++ lib/apolloClient.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-  uri: 'http://localhost:3000/api/graphql',
+  uri: 'https://eyeofthetyphoon.vercel.app/api/graphql',
   cache: new InMemoryCache({}),
   connectToDevTools: true,
 });
