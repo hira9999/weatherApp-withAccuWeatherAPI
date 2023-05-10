ae0b73e0b5749c241023140a69f6c8d149e84ffb
hira9999 <iedddd@naver.com>
2023-05-10 19:13:31 +0900

    change dir public to public/asset

--- components/Common/WeatherItemBG/Sun.tsx
+++ components/Common/WeatherItemBG/Sun.tsx
@@ -8,7 +8,7 @@ const Sun = ({ UVindex }: SunProps) => {
   return (
     <div className="absolute w-full h-full overflow-hidden top-0 opacity-50">
       <img
-        src="sunlight.png"
+        src="/asset/sunlight.png"
         alt="sunlight"
         className="absolute w-full h-[30%] sm:h-full opacity-100 scale-x-[-1]"
         style={{ opacity: `0.${UVindex}` }}
