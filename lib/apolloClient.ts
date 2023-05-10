afaa61b5c2c3a18414eba25d46342dd6156d36f5
hira9999 <iedddd@naver.com>
2023-05-10 20:01:23 +0900

    fix: fix cors error

--- lib/apolloClient.ts
+++ lib/apolloClient.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-  uri: 'https://eyeofthetyphoon.vercel.app/api/graphql',
+  uri: 'https://weather-app-with-accu-weather-naummihlv-hira9999.vercel.app/api/graphql',
   cache: new InMemoryCache({}),
   connectToDevTools: true,
 });
